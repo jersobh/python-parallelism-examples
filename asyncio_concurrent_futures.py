5925dc2f15ce8318195d067fcc5d3b656ae3abd1
Jefferson de Andrade Santos <jersobh@users.noreply.github.com>
2019-01-11 10:58:39 -0200

    Concurrent futures
    Add concurrent futures example with a worker poll

---
+++ asyncio_concurrent_futures.py
@@ -0,0 +1,26 @@
+import asyncio
+import time
+import concurrent.futures
+
+
+def hold(sec):
+    print(f'Running for {sec} seconds')
+    time.sleep(sec)
+    return sec
+
+
+async def main():
+    with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
+        futures = {executor.submit(hold, i) for i in range(5)}
+        for future in concurrent.futures.as_completed(futures):
+            data = future.result()
+            print(data)
+
+
+if __name__ == '__main__':
+    _start = time.time()
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(main())
+    loop.run_until_complete(future)
+    print(f"Execution time: { time.time() - _start }")
+    loop.close()
